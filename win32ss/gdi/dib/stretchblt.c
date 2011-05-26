4f2e6e60f1a2f589fbb349bfeada3764db4ecab8
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-05-26 14:15:11 +0000

    [WIN32K] - add better support for font files in EngLoadModuleEx (save name information) - Fix compilation with MSVC - Fix a memory leak when unloading drivers
    svn path=/branches/GSoC_2011/GdiFontDriver/; revision=51923

--- win32ss/gdi/dib/stretchblt.c
+++ win32ss/gdi/dib/stretchblt.c
@@ -36,6 +36,7 @@ BOOLEAN DIB_XXBPP_StretchBlt(SURFOBJ *DestSurf, SURFOBJ *SourceSurf, SURFOBJ *Ma
   ULONG Dest, Source = 0, Pattern = 0;
   ULONG xxBPPMask;
   BOOLEAN CanDraw;
+  BOOL UsesSource, UsesPattern;
 
   PFN_DIB_GetPixel fnSource_GetPixel = NULL;
   PFN_DIB_GetPixel fnDest_GetPixel = NULL;
@@ -45,8 +46,8 @@ BOOLEAN DIB_XXBPP_StretchBlt(SURFOBJ *DestSurf, SURFOBJ *SourceSurf, SURFOBJ *Ma
 
   LONG PatternX = 0, PatternY = 0;
 
-  BOOL UsesSource = ROP4_USES_SOURCE(ROP);
-  BOOL UsesPattern = ROP4_USES_PATTERN(ROP);
+  UsesSource = ROP4_USES_SOURCE(ROP);
+  UsesPattern = ROP4_USES_PATTERN(ROP);
 
   ASSERT(IS_VALID_ROP4(ROP));
 
